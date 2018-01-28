828af2592a106afb1bce98ec57a453c747d30131
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-01-28 15:06:08 -0700

    Refactor fadeAbout function with jQuery's .each method, delay, and fadeIn

--- scripts.js
+++ scripts.js
@@ -2,7 +2,9 @@ $(document).ready(function() {
   $(window).scroll(function() {
 
     if ($(window).scrollTop() > 300) {
-
+      $(".about").each(function(index) {
+        $(this).delay((index++) * 500).fadeIn("slow");
+      });
     }
 
     if ($(window).scrollTop() > 1900) {
